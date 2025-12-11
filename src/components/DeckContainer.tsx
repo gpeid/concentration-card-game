a579d8092cd09e47ffe2f0a0099ea24561f42a29
George Diep <ghdiep@gmail.com>
2025-12-11 17:36:36 -0500

    refactor: clean up imports and improve formatting in DeckContainer component

--- src/components/DeckContainer.tsx
+++ src/components/DeckContainer.tsx
@@ -1,7 +1,8 @@
-import { useState } from 'react';
-import Card from './Card';
+import { useState } from "react";
+import Card from "./Card";
 import "./../css/DeckContainer.css";
-import type { CardProperties } from '../types/types';
+import type { CardProperties } from "../types/types";
+import ScoreCard from "./ScoreCard";
 
 interface DeckContainerProps {
   deck: CardProperties[];
@@ -9,55 +10,65 @@ interface DeckContainerProps {
 
 const DeckContainer: React.FC<DeckContainerProps> = ({ deck }) => {
   // Array of all matched pairs
-  const [arrayOfSelectedMatches, setArrayOfSelectedMatches] = useState<CardProperties[][]>([]);
+  const [arrayOfSelectedMatches, setArrayOfSelectedMatches] = useState<
+    CardProperties[][]
+  >([]);
   // array of selected paira
   const [pairArray, setPairArray] = useState<CardProperties[]>([]);
-  const [deckCopy, setDeckCopy] = useState<CardProperties[]>(deck.slice())
-  console.log('deck', deck)
-  console.log(deckCopy)
+  const [deckCopy, setDeckCopy] = useState<CardProperties[]>(deck.slice());
 
   const handleSelectCardClick = (selectedCard: CardProperties): void => {
-    const alreadySelected = pairArray.find((card) => card.id === selectedCard.id);
+    const alreadySelected = pairArray.find(
+      (card) => card.id === selectedCard.id
+    );
     if (pairArray.length < 2 && !alreadySelected) {
-      const matchingCardsFound: CardProperties | undefined = pairArray.find(item => item.rank === selectedCard.rank);
-      console.log('matching', matchingCardsFound);
+      const matchingCardsFound: CardProperties | undefined = pairArray.find(
+        (item) => item.rank === selectedCard.rank
+      );
       if (matchingCardsFound) {
         const arrayOfMatchedPairs = [...pairArray, selectedCard];
         setArrayOfSelectedMatches([
           ...arrayOfSelectedMatches,
-          ...[arrayOfMatchedPairs]
+          arrayOfMatchedPairs,
         ]);
 
         arrayOfMatchedPairs.forEach((item) => {
-          const i = deck.findIndex(card => card.id === item.id);
-          console.log(i);
+          const i = deck.findIndex((card) => card.id === item.id);
           deckCopy.splice(i, 1, {
-            id: '',
-            suit: '',
-            rank: '',
-            label: ''
-          })
-        })
+            id: "",
+            suit: "",
+            rank: "",
+            label: "",
+          });
+        });
         setDeckCopy(deckCopy);
-        setPairArray([])
+        setPairArray([]);
       } else {
-        setPairArray([
-          ...pairArray,
-          selectedCard
-        ])
+        setPairArray([...pairArray, selectedCard]);
       }
     } else if (pairArray.length === 2) {
-      setPairArray([
-        selectedCard
-      ])
+      setPairArray([selectedCard]);
     }
-  }
+  };
 
   return (
-    <div className="card_container grid grid-cols-[repeat(12,minmax(0,1fr))_1fr]">
-      {deck.map((card: CardProperties) => (
-        <Card selectCardClick={() => handleSelectCardClick(card)} key={`${card.suit}-${card.rank}`} details={card} toggleSelected={!!pairArray.find(item => item.id === card.id)} matchedCard={deckCopy.findIndex(cardCopy => cardCopy.id === card.id) === -1} />
-      ))}
+    <div className="deck_container relative grid grid-cols-12">
+      <div className="col-span-1">
+        <ScoreCard matches={arrayOfSelectedMatches} />
+      </div>
+      <div className="card_container grid grid-cols-[repeat(12,minmax(0,1fr))_1fr] col-span-11 gap-2">
+        {deck.map((card: CardProperties) => (
+          <Card
+            selectCardClick={() => handleSelectCardClick(card)}
+            key={`${card.suit}-${card.rank}`}
+            details={card}
+            toggleSelected={!!pairArray.find((item) => item.id === card.id)}
+            matchedCard={
+              deckCopy.findIndex((cardCopy) => cardCopy.id === card.id) === -1
+            }
+          />
+        ))}
+      </div>
     </div>
   );
 };
