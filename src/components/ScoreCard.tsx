ca2ae96dfe5ff7a667842042b6b0d1cfd7c7e2eb
George Diep <ghdiep@gmail.com>
2025-12-15 23:27:11 -0500

    feat: enhance ScoreCard component to display matched pairs with color-coded suits

--- src/components/ScoreCard.tsx
+++ src/components/ScoreCard.tsx
@@ -5,13 +5,29 @@ interface ScoreCardProps {
 }
 
 const ScoreCard: React.FC<ScoreCardProps> = ({ matches }) => {
+  const textColorFromSuit = (suitLabel?: string) => {
+    if (suitLabel === "Hearts" || suitLabel === "Diamonds") {
+      return "text-red-600";
+    } else {
+      return "bg-black text-white";
+    }
+  };
+
   return (
     <div className="score_card p-1 mb-5 border border-black rounded">
       <h2>Matched Pairs: {matches.length}</h2>
       <ul>
         {matches.map((pair) => (
           <li key={`${pair[0].id}-${pair[1].id}`}>
-            {pair.map((card) => card.label).join(" and ")}
+            {pair.map((card, i) => (
+              <span
+                key={card.id}
+                className={textColorFromSuit(card.suit?.label)}
+              >
+                {card.label}
+                {i < pair.length - 1 ? " and " : ""}
+              </span>
+            ))}
           </li>
         ))}
       </ul>
